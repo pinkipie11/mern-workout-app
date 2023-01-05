ee35b0109f48522a7be9e0333fe682ff37e7697f
pinkipie11 <85274828+pinkipie11@users.noreply.github.com>
2023-01-05 10:32:32 +0100

    Email and Password validation (JWT)

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -17,9 +18,20 @@ const userSchema = new Schema({
 
 // static signup method
 userSchema.statics.signup = async function (email, password) {
-  const exist = await this.findOne({ email });
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled');
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid');
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough');
+  }
 
-  if (exist) {
+  const exists = await this.findOne({ email });
+
+  if (exists) {
     throw Error('Email already in use');
   }
 
@@ -27,6 +39,26 @@ userSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+
+  return user;
+};
+
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error('All fields must be filled');
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error('Incorrect email');
+  }
+
+  // comparing plain text and hashed password
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error('Incorrect password');
+  }
 
   return user;
 };
