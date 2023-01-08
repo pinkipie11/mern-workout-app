470d552155cae3ee9404867b07f7c91028c9780b
pinkipie11 <85274828+pinkipie11@users.noreply.github.com>
2023-01-08 09:32:03 +0100

    useLogin Hook

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setIsLoading(true);
+    setError(true);
+
+    const response = await fetch('/api/user/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+    // this response is going to return us some info with the jwt
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+    if (response.ok) {
+      // save the user to local storage(email, jwt)
+      localStorage.setItem('user', JSON.stringify(json));
+
+      // update the auth context
+      dispatch({ type: 'LOGIN', payload: json });
+
+      setIsLoading(false);
+    }
+  };
+
+  return { login, isLoading, error };
+};
+
+export default useLogin;
