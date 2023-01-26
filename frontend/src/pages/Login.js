1e67eb3eba752760b36da01db7173b26da58baa3
pinkipie11 <stani.simic22@gmail.com>
2023-01-26 15:36:13 +0100

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,15 @@
 import { useState } from 'react';
+import { useLogin } from "../hooks/useLogin"
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const {login, error, isLoading} = useLogin()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(email, password);
+    await login(email, password)
   };
 
   return (
